e137f8092936385bdf0dc889adb1d1fce940de07
Sudheesh2409 <146453366+Sudheesh2409@users.noreply.github.com>
2025-07-03 16:24:55 +0530

    final commit 1

--- src/route.js
+++ src/route.js
@@ -1,22 +1,26 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Collection from "./pages/collection"
-import Home from "./pages/home"
-import About from "./pages/about" 
-import Contact from "./pages/contact"
-import Stores from "./pages/stores"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Collection from "./pages/collection";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Stores from "./pages/stores";
+import ScrollToTop from "./ScrollToTop"; // <-- Import the ScrollToTop component
 
 function RoutesHandler() {
-    return ( <div>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/collection" element={<Collection/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/contact" element={<Contact/>} />
-                <Route path="/stores" element={<Stores/>} />
-            </Routes>
-        </BrowserRouter>
-    </div> );
+    return (
+        <div>
+            <BrowserRouter>
+                <ScrollToTop /> {/* <-- Add this line above your Routes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/collection" element={<Collection />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/stores" element={<Stores />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default RoutesHandler;
